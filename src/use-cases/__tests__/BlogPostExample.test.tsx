9987d0fadefbe06813dde7292fba2985419c03db
JenniferFuBook <jenniferfu888@gmail.com>
2025-04-19 21:13:34 -0700

    Added a snapshot test

--- src/use-cases/__tests__/BlogPostExample.test.tsx
+++ src/use-cases/__tests__/BlogPostExample.test.tsx
@@ -2,13 +2,20 @@ import { render, screen } from '@testing-library/react';
 import BlogPostExample from '../BlogPostExample';
 
 describe('BlogPostExample', () => {
+  it("matches snapshot", () => { // This is a snapshot test
+    const { container } = render(<BlogPostExample />);
+    expect(container).toMatchSnapshot();
+  });
+
   it('renders the blog post title', () => {
+    // This test checks if the title of the blog post is rendered correctly
     render(<BlogPostExample />);
     const titleElement = screen.getByText(/My Favorite Article/i);
     expect(titleElement).toBeInTheDocument();
   });
 
-  it('renders the blog post content', () => {
+  it('renders the blog post content', () => { 
+    // This test checks if the content of the blog post is rendered correctly
     render(<BlogPostExample />);
     const contentElement = screen.getByText(/React is a JavaScript library/i);
     expect(contentElement).toBeInTheDocument();
